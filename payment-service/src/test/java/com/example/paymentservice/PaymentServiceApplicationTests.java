5dbd8a4c96ba2b4d3be319bcc1f1f6c59980fc6c
Denis <denis@krutov.com>
2025-11-20 16:59:44 +0300

    payment test5

--- payment-service/src/test/java/com/example/paymentservice/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/example/paymentservice/PaymentServiceApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.paymentservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class PaymentServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(2, 1 + 1);
 	}
 
 }
