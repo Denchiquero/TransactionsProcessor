9bb8fe300489ab147d067ea74c970fbc8991ec39
Denis <denis@krutov.com>
2025-11-20 16:56:42 +0300

    payment test3

--- order-service/src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class OrderServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(2, 1 + 1);
 	}
 
 }
